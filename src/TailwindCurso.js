28bef5bc96ad730cc43fa9e1af99f777017a0d19
JLS <JLS@chozas.lan>
2021-10-06 09:34:42 -0300

    actualizacion

---
+++ src/TailwindCurso.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TailwindCurso = () => {
+	return (
+		<div>
+			<h1 className="">Hola mundo</h1>
+			<p className="bg-yellow-500">soy un parrafo</p>
+		</div>
+	)
+}
